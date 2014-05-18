028a8121a289223079624f41fc2f00d586338305
bubblebenj <bubblebenj@2f029e58-c93a-b8c8-1446-a049876ca326>
2014-05-18 06:09:23 +0000

    move to haxe 3

--- branches/picogp/Src/lib/math/CHexagone.hx
+++ branches/picogp/Src/lib/math/CHexagone.hx
@@ -5,20 +5,19 @@
  * @author bdubois
  */
 
-class CHexagone 
+class CHexagone
 {
 	/*
 	 * la hauteur d'un triangle isocele = x * ( sqrt(3)/2 ) où x est la longueur d'un côté.
 	 */
-	public static inline var m_IsoceleSideToHeightCoef	: Float	= (Math.sqrt(3) / 2); 
+	public static var m_IsoceleSideToHeightCoef( default, never ) : Float	= { (Math.sqrt(3) / 2); }
 	 /*
 	  * donc la longueur d'un côté vaut x * ( 2*sqrt(3)/3 ) où x est la hauteur du triangle (i.e. x == m_CellSize ).
 	  */
-	public static inline var m_IsoceleHeightToSideCoef	: Float	= (2 * Math.sqrt(3) / 3);
+	public static var m_IsoceleHeightToSideCoef( default, never ) : Float	= { (2 * Math.sqrt(3) / 3); }
 
-	
-	public function new() 
+	public function new()
 	{
 	}
-	
+
 }
--- branches/picogp/Src/lib/tools/transition/CTween.hx
+++ branches/picogp/Src/lib/tools/transition/CTween.hx
@@ -18,20 +18,20 @@ enum TWIN_STATE
 }
 
 class CTween
-{	
+{
 	public static var m_TweenList	: GenericStack<CTween> = new GenericStack<CTween>();
-	
+
 	public	var m_State				: TWIN_STATE;
-	
+
 	public	var m_Handler			: Dynamic -> Void;
 	public	var	m_StartVal			: Dynamic;
 	public	var m_EndValue			: Dynamic;
 	public	var	m_Duration			: Int;
 	public	var m_VariationCallback	: Int -> Dynamic -> Dynamic -> Int -> Dynamic;
 	public	var m_EndFunction		: Void -> Dynamic;
-	
+
 	public function new( _Handler : Dynamic -> Void, _StartVal : Dynamic, _EndValue : Dynamic,
-						_Duration : Int, _VariationFunction : Int -> Dynamic -> Dynamic -> Int -> Dynamic, ? _EndFunction : Void -> Dynamic ) 
+						_Duration : Int, _VariationFunction : Int -> Dynamic -> Dynamic -> Int -> Dynamic, ? _EndFunction : Void -> Dynamic )
 	{
 		m_Handler			= _Handler;
 		m_StartVal			= _StartVal;
@@ -40,68 +40,68 @@ class CTween
 		m_VariationCallback	= _VariationFunction;
 		m_EndFunction		= _EndFunction;
 		InitCounter();
-		
+
 		Stop();
 		m_TweenList.add( this );
 	}
-	
+
 	//private var m_Obj				: Dynamic;
 	//private var	m_Field				: String;
 
-	
+
 	private var m_FrameCounter		: Int;
-	
+
 	public inline function Start()
 	{
 		m_State	= TW_STARTED;
 	}
-	
+
 	public inline function Stop()
 	{
 		m_State	= TW_STOPPED;
 		InitCounter();
 	}
-	
+
 	public inline function Pause()
 	{
 		m_State	= TW_PAUSED;
 	}
-	
+
 	public inline function InitCounter()	: Void
 	{
 		m_FrameCounter	= 0;
 	}
-	
+
 	public inline function GetValue( _Time : Int )	: Dynamic
 	{
 		return m_VariationCallback( _Time, m_StartVal, m_EndValue, m_Duration );
-	}	
-	
+	}
+
 	public inline function GetCurrentTime()	: Int
 	{
 		return Math.floor( m_FrameCounter * Glb.GetSystem().GetDeltaTime() * 1000 );
 	}
-	
+
 	public inline function GetCurrentValue()	: Dynamic
 	{
 		return GetValue( GetCurrentTime() );
 	}
-	
+
 	public inline function IsFinished()		: Bool
 	{
 		return m_State == TW_FINISHED;
 	}
-	
+
 	public inline function IsPlaying()		: Bool
 	{
 		return m_State == TW_STARTED;
 	}
-	
+
 	public inline function SetEndFunction( _EndFunction : Void -> Dynamic )
 	{
 		m_EndFunction		= _EndFunction;
 	}
-	
+
 	public inline function ContinueTo( _EndValue : Dynamic, _Duration : Int )
 	{
 		m_StartVal	= GetCurrentValue();
@@ -109,18 +109,18 @@ class CTween
 		m_Duration	= _Duration;
 		m_EndValue	= _EndValue;
 	}
-	
+
 	/* Go to the specified position [ms] (in ms) */
 	public inline function Seek( _ms : Int ) : Void
 	{
 		m_FrameCounter = cast( _ms * Glb.GetSystem().GetFrameRate() * 0.001, Int );
 	}
-	
+
 	public function Remove() : Void
 	{
 		m_TweenList.remove( this );
 	}
-	
+
 	public function Update()
 	{
 		switch ( m_State )
@@ -130,7 +130,7 @@ class CTween
 				m_FrameCounter++;
 				//trace( GetCurrentTime() + " = " + m_FrameCounter + " * " + Glb.GetSystem().GetDeltaTime() * 1000 );
 				var l_Value : Dynamic;
-				
+
 				if ( GetCurrentTime() < m_Duration )
 				{
 					l_Value = GetCurrentValue();
@@ -156,7 +156,7 @@ class CTween
 			}
 			default :
 			{
-				
+
 			}
 		}
 	}
